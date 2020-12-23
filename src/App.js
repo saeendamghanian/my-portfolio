34266dafdc20d1409693b7236963f0bd4de7f20f
Saeen Damghanian <saeendamghanian@Saeens-MacBook-Pro.local>
2020-12-23 16:28:39 +0100

    End of Testimonial

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 import './App.css';
 import About from './Components/About';
+import Testimonial from './Components/Testimonial';
 
 function App() {
-  return (
-    <div className='App'>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<NavBar />
+			<Home />
+			<About />
+			<Testimonial />
+		</div>
+	);
 }
 
 export default App;
--- src/Components/About/AboutMe.js
+++ src/Components/About/AboutMe.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardActions, CardContent, CardMedia, Button, Typography, Container, Box } from '@material-ui/core';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, Box } from '@material-ui/core';
 import bg from '../../images/image_1.jpg';
 
 const useStyles = makeStyles(theme => ({
